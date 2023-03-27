a26e73e256418f3ae2e1474cc4dfa17d259e3f45
xingzai <447166939@qq.com>
2023-03-28 05:12:16 +0800

    update

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import styles from './test.module.scss'
-export interface TestProps{}
-const Test:React.FC<TestProps>=()=>{
-    return (
-        <div className={styles.container}>
-
-        </div>
-    )
-}
+import React from "react";
+import styles from "./test.module.scss";
+export interface TestProps {}
+const Test: React.FC<TestProps> = () => {
+  return <div className={styles.container}></div>;
+};
 export default Test;
